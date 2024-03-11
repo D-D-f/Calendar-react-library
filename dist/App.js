d767ace0a5afd52da96d051891ba669a273a847d
D-D-f <defreitas-david@outlook.fr>
2024-03-11 18:37:43 +0100

    update version

--- dist/App.js
+++ dist/App.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _ContainerTable = _interopRequireDefault(require("./Component/ContainerTable/ContainerTable.jsx"));
-var _ActiveContext = _interopRequireDefault(require("./Context/ActiveContext.tsx"));
+var _ContainerTable = _interopRequireDefault(require("../lib/Component/ContainerTable/ContainerTable.jsx"));
+var _ActiveContext = _interopRequireDefault(require("./Context/ActiveContext.jsx"));
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const App = () => {
