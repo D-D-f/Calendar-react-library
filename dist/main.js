7ef27055810a0b1ccd5005da4925ff8242fcb320
D-D-f <defreitas-david@outlook.fr>
2024-03-11 12:52:13 +0100

    update version

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _reactDom = _interopRequireDefault(require("react-dom"));
-var _App = _interopRequireDefault(require("./App"));
-require("./index.css");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_reactDom.default.render( /*#__PURE__*/React.createElement(_App.default, null), document.getElementById("root"));
